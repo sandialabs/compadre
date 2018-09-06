3f2f7601346d05e6f303edc53b0da8b8d7ca96c9
Paul Kuberry <pakuber@sandia.gov>
2018-09-06 08:17:01 -0600

    Made the build system more robust with respect to VTK and Trilinos libraries that need to be linked against in the install of the toolkit.

--- example_executable/build/run_program.sh
+++ example_executable/build/run_program.sh
@@ -1,3 +1,3 @@
 # make sure that you have first built the program before running this script
-mpirun -n 4 ./tpetraCoordsTest.exe --i=../parameters/parameters_amg.xml
+mpirun -n 4 ./tpetraCoordsTest.exe --i=../parameters/parameters_amg.xml --kokkos-threads=2
 
