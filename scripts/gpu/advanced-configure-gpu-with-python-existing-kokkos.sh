b02e1e9e599fe5fb51d954ffbc7a4f1283ec31b1
Paul Kuberry <pakuber@sandia.gov>
2019-03-08 12:05:58 -0700

    Added a custom rescan target if KokkosCore_PREFIX is not specified.
    Now using CMake to propagate variables into configuration of KokkosCore.
    
    Configure is now run on nvcc_wrapper to set it to whatever compiler the user specifies.
    
    For existing Kokkos installations, their enabling of various devices is now checked
    against the Compadre_USE_ for consistency.

--- scripts/gpu/advanced-configure-gpu-with-python-existing-kokkos.sh
+++ scripts/gpu/advanced-configure-gpu-with-python-existing-kokkos.sh
@@ -5,7 +5,7 @@
 
 # Cuda on GPU via Kokkos
 # With Python interface
-# Standalone Kokkos
+# Standalone Kokkos already built
 
 # following lines for build directory cleanup
 find . ! -name '*.sh' -type f -exec rm -f {} +
@@ -13,25 +13,22 @@ find . -mindepth 1 -type d -exec rm -rf {} +
 
 # make sure you installed kokkos somewhere before setting this
 # see doc/Kokkos-Install.md for details on installing Kokkos
-MY_KOKKOSCORE_PREFIX="../../kokkos/build/install-openmp"
-
-# pick your favorite c++ compiler
-MY_CXX_COMPILER=`which g++`
+MY_KOKKOSCORE_PREFIX="$HOME/releases/kokkos/install/pascal60"
 
 # this will install in your build directory in a folder called install by default
-INSTALL_PREFIX="./install" 
+INSTALL_PREFIX="./install"
 
 # optional CMake variables to pass in are PYTHON_PREFIX and PYTHON_EXECUTABLE
 # if they are not passed in, then `which python` is called to determine your
 # python executable, and from that sitepackages and libraries are inferred.
-
-# Compadre_USE_LAPACK set to off because there is no point in searching for
-# LAPACK libraries when we are using Cuda toolkit libraries for dense solves
 cmake \
     -D CMAKE_CXX_COMPILER="$MY_KOKKOSCORE_PREFIX/bin/nvcc_wrapper" \
-    -D CMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" \
     -D KokkosCore_PREFIX="$MY_KOKKOSCORE_PREFIX" \
+    -D CMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" \
     -D Compadre_USE_PYTHON:BOOL=ON \
     -D Compadre_USE_LAPACK:BOOL=OFF \
+    -D Compadre_USE_CUDA:BOOL=ON \
+    -D CUDA_PREFIX:PATH="/home/projects/ppc64le-pwr8-nvidia/cuda/9.2.88" \
     \
     ..
+
