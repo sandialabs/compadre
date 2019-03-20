feb136f7c4929852d309a8d080a0696a3d605ca1
Paul Kuberry <pkuberry@gmail.com>
2019-03-20 16:13:23 -0600

    Fixed linker flags for Python calling build on the toolkit with a given OSX_DEPLOYMENT_TARGET.

--- python/run_packaging.sh
+++ python/run_packaging.sh
@@ -7,25 +7,27 @@ cp pyproject.toml ..
 # step down to root of repo
 cd ..
 
+rm -rf compadre.egg-info
+
 python setup.py bdist_wheel sdist
 echo "bdist_wheel and sdist complete."
 
-rm -rf build/ && rm -rf python_src/CMakeFiles && rm -rf python_src/CMakeCache.txt && cd dist && tar -xzvf compadre-$1.tar.gz && cd compadre-$1 && cp PKG-INFO ../.. && cd ../.. && rm -rf dist 
-echo "files copied from dist."
-
-tar -czvf compadre-$1.tar.gz *
-echo "files tar balled."
-
-mkdir dist && mv compadre-$1.tar.gz dist
-echo "tar ball moved to dist."
-
-# test pypi site
+#rm -rf build/ && cd dist && tar -xzvf compadre-$1.tar.gz && cd compadre-$1 && cp PKG-INFO ../.. && cd ../.. && rm -rf dist 
+#echo "files copied from dist."
+#
+#tar -czvf compadre-$1.tar.gz *
+#echo "files tar balled."
+#
+#mkdir dist && mv compadre-$1.tar.gz dist
+#echo "tar ball moved to dist."
+#
+## test pypi site
 #python -m twine upload --repository-url https://test.pypi.org/legacy/ dist/* --verbose
 #echo "uploaded to test pypi"
 
 # real pypi site
-twine upload dist/*
-echo "uploaded to real pypi"
+#twine upload dist/*
+#echo "uploaded to real pypi"
 
 # step back up to calling folder
 cd python
