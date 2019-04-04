4d3b087f7db77e0747b164277de0ed0580e6630c
Paul Kuberry <pkuberry@gmail.com>
2019-04-04 09:45:59 -0500

    Extracted kdtree construction from the neighbor search, allowing reuse of the kdtree for multiple searches with different target sites.
    Moved to managing kdtree with a smart pointer rather than manually
    allocating and deallocating memory.

--- src/Compadre_PointCloudSearch.hpp
+++ src/Compadre_PointCloudSearch.hpp
@@ -1,6 +1,7 @@
 #include <tpl/nanoflann.hpp>
 #include <Kokkos_Core.hpp>
 #include "Compadre_Typedefs.hpp"
+#include <memory>
 
 namespace Compadre {
 
@@ -102,23 +103,23 @@ class PointCloudSearch {
 
     protected:
 
+        // define the cloud_type using this class
+        typedef nanoflann::KDTreeSingleIndexAdaptor<nanoflann::L2_Simple_Adaptor<double, PointCloudSearch<view_type> >, 
+                PointCloudSearch<view_type>, 3> tree_type;
+
+
         //! source site coordinates
         view_type _src_pts_view;
 
-        //! target site coordinates
-        view_type _trg_pts_view;
-
     public:
 
-        PointCloudSearch(view_type src_pts_view, view_type trg_pts_view) 
-                : _src_pts_view(src_pts_view), _trg_pts_view(trg_pts_view) {
+        PointCloudSearch(view_type src_pts_view) 
+                : _src_pts_view(src_pts_view) {
             compadre_assert_release((std::is_same<typename view_type::memory_space, Kokkos::HostSpace>::value) &&
                     "Views passed to PointCloudSearch at construction should reside on the host.");
-            compadre_assert_release((src_pts_view.dimension_1()==3 && trg_pts_view.dimension_1()==3) &&
+            compadre_assert_release(src_pts_view.dimension_1()==3 &&
                     "Views passed to PointCloudSearch at construction must have second dimension of 3.");
         };
-
-        PointCloudSearch(view_type src_pts_view) : PointCloudSearch(src_pts_view, src_pts_view) {};
     
         ~PointCloudSearch() {};
 
@@ -149,35 +150,39 @@ class PointCloudSearch {
 
         }
 
-        //! Generates neighbor lists
-        template <typename neighbor_lists_view_type, typename epsilons_view_type>
-        void generateNeighborListsFromKNNSearch(neighbor_lists_view_type neighbor_lists,
-                epsilons_view_type epsilons, const int neighbors_needed, 
-                const int dimension = 3, const double epsilon_multiplier = 1.6, bool max_search_radius = 0.0) {
-
-            compadre_assert_release((std::is_same<typename neighbor_lists_view_type::memory_space, Kokkos::HostSpace>::value) &&
-                    "Views passed to generateNeighborListsFromKNNSearch should reside on the host.");
-            compadre_assert_release((std::is_same<typename epsilons_view_type::memory_space, Kokkos::HostSpace>::value) &&
-                    "Views passed to generateNeighborListsFromKNNSearch should reside on the host.");
-
-            // define the cloud_type using this class
-            typedef nanoflann::KDTreeSingleIndexAdaptor<nanoflann::L2_Simple_Adaptor<double, decltype(*this)>, 
-                    decltype(*this), 3> tree_type;
-
-            // default search parameters
-            nanoflann::SearchParams search_parameters; 
+        std::shared_ptr<tree_type> generateKDTree(const int dimension = 3) {
 
             // maximum number of levels in the tree constructed by nanoflann
             const int maxLeaf = 10;
 
             // allocate a kd_tree from nanoflann
-            tree_type *kd_tree = new tree_type(dimension, *this, nanoflann::KDTreeSingleIndexAdaptorParams(maxLeaf));
+            std::shared_ptr<tree_type> kd_tree(new tree_type(dimension, *this, nanoflann::KDTreeSingleIndexAdaptorParams(maxLeaf)));
 
             // insert points to build up the tree
             kd_tree->buildIndex();
 
+            return kd_tree;
+
+        }
+
+        //! Generates neighbor lists
+        template <typename neighbor_lists_view_type, typename epsilons_view_type>
+        void generateNeighborListsFromKNNSearch(view_type trg_pts_view, neighbor_lists_view_type neighbor_lists,
+                epsilons_view_type epsilons, const int neighbors_needed, 
+                const int dimension = 3, const double epsilon_multiplier = 1.6, std::shared_ptr<tree_type> kd_tree = NULL, bool max_search_radius = 0.0) {
+
+            compadre_assert_release(trg_pts_view.dimension_1()==3 &&
+                    "Views passed to PointCloudSearch at construction must have second dimension of 3.");
+            compadre_assert_release((std::is_same<typename neighbor_lists_view_type::memory_space, Kokkos::HostSpace>::value) &&
+                    "Views passed to generateNeighborListsFromKNNSearch should reside on the host.");
+            compadre_assert_release((std::is_same<typename epsilons_view_type::memory_space, Kokkos::HostSpace>::value) &&
+                    "Views passed to generateNeighborListsFromKNNSearch should reside on the host.");
+
             // loop size
-            const int num_target_sites = _trg_pts_view.dimension_0();
+            const int num_target_sites = trg_pts_view.dimension_0();
+
+            if (!kd_tree)
+                kd_tree = this->generateKDTree(dimension);
 
             // allocate neighbor lists and epsilons
             compadre_assert_release((neighbor_lists.dimension_0()==num_target_sites 
@@ -226,7 +231,7 @@ class PointCloudSearch {
                     // target_coords is LayoutLeft on device and its HostMirror, so giving a pointer to 
                     // this data would lead to a wrong result if the device is a GPU
 
-                    for (int j=0; j<3; ++j) this_target_coord(j) = _trg_pts_view(i,j);
+                    for (int j=0; j<3; ++j) this_target_coord(j) = trg_pts_view(i,j);
 
                     neighbors_found(0) = kd_tree->knnSearch(this_target_coord.data(), neighbors_needed, 
                             neighbor_indices.data(), neighbor_distances.data()) ;
@@ -279,7 +284,7 @@ class PointCloudSearch {
                     // target_coords is LayoutLeft on device and its HostMirror, so giving a pointer to 
                     // this data would lead to a wrong result if the device is a GPU
 
-                    for (int j=0; j<3; ++j) this_target_coord(j) = _trg_pts_view(i,j);
+                    for (int j=0; j<3; ++j) this_target_coord(j) = trg_pts_view(i,j);
 
                     nanoflann::SearchParams sp; // default parameters
                     Compadre::RadiusResultSet<double> rrs(epsilons(i)*epsilons(i), neighbor_distances.data(), neighbor_indices.data(), neighbor_lists.dimension_1());
@@ -305,9 +310,6 @@ class PointCloudSearch {
 
             });
             Kokkos::fence();
-
-            // deallocate nanoflann kdtree
-            free(kd_tree);
         }
 
 }; // PointCloudSearch
