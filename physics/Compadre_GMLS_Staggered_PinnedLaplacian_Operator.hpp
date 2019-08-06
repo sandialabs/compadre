5400acb2c082cfb5a098c3fdda142f7b6df3d3cc
Quang Ha <qth20@bu.edu>
2019-08-06 16:56:57 -0400

    Add staggered operator

---
+++ physics/Compadre_GMLS_Staggered_PinnedLaplacian_Operator.hpp
@@ -0,0 +1,33 @@
+#ifndef _COMPADRE_GMLS_STAGGERED_PINNEDLAPLACIANPHYSICS_HPP_
+#define _COMPADRE_GMLS_STAGGERED_PINNEDLAPLACIANPHYSICS_HPP_
+
+#include <Compadre_PhysicsT.hpp>
+
+/*
+ Constructs the matrix operator.
+
+ */
+namespace Compadre {
+
+class ParticlesT;
+
+class GMLS_Staggered_LaplacianPhysics : public PhysicsT {
+
+	protected:
+		typedef Compadre::ParticlesT particle_type;
+		local_index_type Porder;
+
+	public:
+		GMLS_Staggered_LaplacianPhysics(Teuchos::RCP<particle_type> particles, local_index_type t_Porder,
+                                                Teuchos::RCP<crs_graph_type> A_graph = Teuchos::null,
+                                                Teuchos::RCP<crs_matrix_type> A = Teuchos::null) :
+                PhysicsT(particles, A_graph, A), Porder(t_Porder) {}
+
+		virtual ~GMLS_Staggered_LaplacianPhysics() {}
+		virtual Teuchos::RCP<crs_graph_type> computeGraph(local_index_type field_one, local_index_type field_two = -1);
+		virtual void computeMatrix(local_index_type field_one, local_index_type field_two = -1, scalar_type time = 0.0);
+		virtual const std::vector<InteractingFields> gatherFieldInteractions();
+};
+
+}
+#endif
