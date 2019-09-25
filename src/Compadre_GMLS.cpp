f74ede28fe9d7a0e23d6c99c320b3abdace9cb02
Quang Ha <qth20@bu.edu>
2019-09-25 13:20:51 -0600

    Remove comments used during development

--- src/Compadre_GMLS.cpp
+++ src/Compadre_GMLS.cpp
@@ -1049,9 +1049,6 @@ void GMLS::operator()(const AssembleManifoldPsqrtW&, const member_type& teamMemb
         });
     }
 
-    // Quang will put LU here
-    // Q will need conditionally set like Coeffs was
-
     teamMember.team_barrier();
 }
 
