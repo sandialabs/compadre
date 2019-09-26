9f2568ff6731cecb4a2047b484dee750ea2572a2
Quang Ha <qth20@bu.edu>
2019-09-26 08:43:11 -0600

    Working function to convert layout right to layout left matrix

--- src/Compadre_Misc.hpp
+++ src/Compadre_Misc.hpp
@@ -1,6 +1,8 @@
 #ifndef _COMPADRE_MISC_HPP_
 #define _COMPADRE_MISC_HPP_
 
+#include "Compadre_Operators.hpp"
+
 namespace Compadre {
 
 struct XYZ {
@@ -36,6 +38,30 @@ struct XYZ {
         return result;
     }
 }; // XYZ
+
+KOKKOS_INLINE_FUNCTION
+void convertMatrixLayoutRightToLeft(scratch_matrix_right_type global_mat, scratch_matrix_left_type scratch_mat) {
+    // create a layout left view on the global matrix
+    scratch_matrix_left_type global_mat_left(global_mat.data(), global_mat.extent(0), global_mat.extent(1));
+
+    // Create a 1D array view on the global matrix data
+    scratch_vector_type global_mat_left_flat(global_mat_left.data(), global_mat_left.extent(0)*global_mat_left.extent(1));
+    // Create a 1D array view on the scratch matrix data
+    scratch_vector_type scratch_mat_flat(scratch_mat.data(), scratch_mat.extent(0)*scratch_mat.extent(1));
+
+    // Transpose the matrix and copy from the global mat data to scratch mat data
+    for (int i=0; i<scratch_mat.extent(0); i++) {
+        for (int j=0; j<scratch_mat.extent(1); j++) {
+            // Transpose the global matrix and copy into scratch matrix data
+            scratch_mat(i, j) = global_mat_left(j, i);
+        }
+    }
+
+    // Now copy the flat 1D array from scratch matrix data back to the global data
+    for (int i=0; i<scratch_mat.extent(0)*scratch_mat.extent(1); i++) {
+        global_mat_left_flat(i) = scratch_mat_flat(i);
+    }
+}
 
 }; // Compadre
 
