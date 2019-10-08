6f933ec8366451a8548c24e43e6aae00b67700f0
Quang Ha <qth20@bu.edu>
2019-10-08 15:00:12 -0600

    Add a function to calculate the matrix size based on different choices of solver

--- src/Compadre_Misc.hpp
+++ src/Compadre_Misc.hpp
@@ -1,6 +1,8 @@
 #ifndef _COMPADRE_MISC_HPP_
 #define _COMPADRE_MISC_HPP_
 
+#include "Compadre_Operators.hpp"
+
 namespace Compadre {
 
 struct XYZ {
@@ -36,6 +38,18 @@ struct XYZ {
         return result;
     }
 }; // XYZ
+
+KOKKOS_INLINE_FUNCTION
+int getRHSDims(DenseSolverType dense_solver_type, ConstraintType constraint_type, const int M, const int N) {
+    // Return the appropriate size for _RHS. Since in LU, the system solves P^T*P against P^T*W.
+    // We store P^T*P in the RHS space, which means RHS can be much smaller compared to the
+    // case for QR/SVD where the system solves PsqrtW against sqrtW*Identity
+    if (dense_solver_type != LU) {
+        return M;
+    } else {
+        return N;
+    }
+}
 
 }; // Compadre
 
