afe64b3fc222e09bb3cfadae3c40e39966f184e6
Quang Ha <qth20@bu.edu>
2020-01-13 11:13:46 -0500

    Add scalar derivative for Second Order Basis

--- src/Compadre_AnalyticFunctions.hpp
+++ src/Compadre_AnalyticFunctions.hpp
@@ -316,6 +316,8 @@ class SecondOrderBasis : public AnalyticFunction {
 
 		virtual scalar_type evalScalar(const xyz_type& xIn) const;
 
+		virtual xyz_type evalScalarDerivative(const xyz_type& xIn) const;
+
 		virtual xyz_type evalVector(const xyz_type& xIn) const;
 
 		virtual scalar_type evalScalarLaplacian(const xyz_type& xyzIn) const;
