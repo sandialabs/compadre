99edcd30b312b328da2b81e5d94aabbbf10581a6
Paul Kuberry <pkuberry@gmail.com>
2020-02-11 07:33:08 -0700

    Updated NetCDF installation instructions / script.

--- scripts/kuberry/linux/configure-netcdf.sh
+++ scripts/kuberry/linux/configure-netcdf.sh
@@ -1,9 +1,16 @@
-#CC=mpicc ./configure --enable-netcdf-4 -with-gnu-ld --enable-logging \
-#--prefix=$HOME/releases/netcdf \
-#--enable-parallel-tests \
-#LDFLAGS='LIBS=-lhdf5 -lz -ldl -lmpi'
-CC=mpicc CPPFLAGS=-I${HDF5_ROOT}/include LDFLAGS=-L${HDF5_ROOT}/lib \
-./configure --enable-parallel-tests --prefix=/home/pakuber/releases/netcdf
+# get source code at https://www.unidata.ucar.edu/downloads/netcdf/
+# get .tar.gz file under "NetCDF-C Releases"
 
-#CC=mpicc CPPFLAGS=-I${HDF5_ROOT}/include LDFLAGS=-L${HDF5_ROOT}/lib \
-#./configure --disable-shared --enable-parallel-tests --prefix=/home/pakuber/releases/netcdf
+HDF5_ROOT=/your/hdf5
+INSTALL_PREFIX=/your/netcdf
+MPI_DIR=/usr
+
+CC=$MPI_DIR/bin/mpicc CPPFLAGS=-I${HDF5_ROOT}/include LDFLAGS=-L${HDF5_ROOT}/lib \
+./configure --enable-parallel-tests --prefix=$INSTALL_PREFIX --disable-dap
+
+# --disable-dap is so that you don't need curl
+# follow up with 
+# >> make -j4
+# >> make install
+# (optional) >> make check
+
