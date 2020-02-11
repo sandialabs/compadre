e9cb54a3f9f44a0a6add773d450f8cac8dda165d
Paul Kuberry <pkuberry@gmail.com>
2020-02-11 07:17:33 -0700

    Updated hdf5 installation instructions / script.

--- scripts/kuberry/linux/configure-hdf5.sh
+++ scripts/kuberry/linux/configure-hdf5.sh
@@ -1,1 +1,11 @@
-CC=mpicc ./configure --enable-parallel --prefix=/home/pakuber/releases/hdf5
+# source code at https://www.hdfgroup.org/downloads/hdf5/source-code/
+# be SURE to get the NON-cmake version ending in .tar.gz 
+# (.zip has wrong line endings)
+
+INSTALL_PREFIX=/your/favorite/install/location
+
+CC=mpicc ./configure --enable-parallel --prefix=$INSTALL_PREFIX
+
+# follow with 
+# >> make -j4 
+# >> make install
