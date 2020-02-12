7dc4c4e8c73a54ce9966b157e7ccd081eee95e3c
Paul Kuberry <pkuberry@gmail.com>
2020-02-12 14:46:25 -0700

    Updated make_simple_cube_vtk to also add FLAG field and P field.

--- test_data/grids/make_simple_cube_vtk.py
+++ test_data/grids/make_simple_cube_vtk.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # Number of points along each axis
-N = 4
+N = 48
 
 # Minimum and maximum value for coordinates
 min_coord, max_coord = -1.0, 1.0
@@ -26,5 +26,26 @@ for x in X:
     for y in Y:
         for z in Z:
             output_file.write("{:.6f} {:.6f} {:.6f}\n".format(x, y, z))
+
+output_file.write("POINT_DATA %d\n"%(N*N*N))
+output_file.write("SCALARS FLAG float 1\n")
+output_file.write("LOOKUP_TABLE default\n")
+
+for x in X:
+    for y in Y:
+        for z in Z:
+            if ((x==X[0] or x==X[-1]) or (y==Y[0] or y==Y[-1]) or (z==Z[0] or z==Z[-1])):
+                output_file.write("{:d}\n".format(1))
+            else:
+                output_file.write("{:d}\n".format(0))
+
+output_file.write("SCALARS P float 1\n")
+output_file.write("LOOKUP_TABLE default\n")
+
+for x in X:
+    for y in Y:
+        for z in Z:
+            output_file.write("{:.1f}\n".format(0))
+
 
 output_file.close()
