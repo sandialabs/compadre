836ffc0649152d8b21554b92c8ed693261a40adb
Paul Kuberry <pakuber@sandia.gov>
2020-02-12 14:10:38 -0700

    Updated Compadre Toolkit to SHA fdb98351dd289f

--- toolkit/src/Compadre_Utilities.hpp
+++ toolkit/src/Compadre_Utilities.hpp
@@ -85,38 +85,85 @@ Kokkos::View<int*, output_array_layout, output_memory_space> // shares layout of
     return filtered_view_output;
 }
 
-template <typename output_memory_space, typename view_type_input_data, typename view_type_index_data, typename input_data_type = typename view_type_input_data::data_type, typename output_array_layout = typename view_type_input_data::array_layout, typename output_data_type = typename view_type_input_data::data_type>
-Kokkos::View<output_data_type, output_array_layout, output_memory_space> // shares layout of input by default
-        extractViewByIndex(view_type_input_data input_data_host_or_device, view_type_index_data index_data_host_or_device) {
+struct Extract {
 
-    // Make view on the host for input data (does nothing if already on the host)
-    auto input_data_host = Kokkos::create_mirror_view(input_data_host_or_device);
-    Kokkos::deep_copy(input_data_host, input_data_host_or_device);
-    Kokkos::fence();
+    template <typename output_memory_space, typename view_type_input_data, typename view_type_index_data,
+              enable_if_t<std::is_same<typename view_type_input_data::data_type, double**>::value
+                  ||std::is_same<typename view_type_input_data::data_type, int**>::value, int> = 0>
+    static Kokkos::View<typename view_type_input_data::data_type, typename view_type_input_data::array_layout, output_memory_space> // shares layout of input by default
+            extractViewByIndex(view_type_input_data input_data_host_or_device, view_type_index_data index_data_host_or_device) {
 
-    // Make view on the host for index data (does nothing if already on the host)
-    auto index_data_host = Kokkos::create_mirror_view(index_data_host_or_device);
-    Kokkos::deep_copy(index_data_host, index_data_host_or_device);
-    Kokkos::fence();
-
-    // Create a new view to extract out the rows that belong to the filtered index
-    Kokkos::View<output_data_type, output_array_layout, output_memory_space> extracted_view("extracted view", index_data_host.extent(0), input_data_host.extent(1));
-
-    // Loop through all the entries of index data
-    for (int i=0; i<index_data_host.extent(0); i++) {
-        for (int j=0; j<input_data_host.extent(1); j++) {
-            extracted_view(i, j) = input_data_host(index_data_host(i), j);
+        typedef typename view_type_input_data::data_type    output_data_type;
+        typedef typename view_type_input_data::array_layout output_array_layout;
+    
+        // Make view on the host for input data (does nothing if already on the host)
+        auto input_data_host = Kokkos::create_mirror_view(input_data_host_or_device);
+        Kokkos::deep_copy(input_data_host, input_data_host_or_device);
+        Kokkos::fence();
+    
+        // Make view on the host for index data (does nothing if already on the host)
+        auto index_data_host = Kokkos::create_mirror_view(index_data_host_or_device);
+        Kokkos::deep_copy(index_data_host, index_data_host_or_device);
+        Kokkos::fence();
+    
+        // Create a new view to extract out the rows that belong to the filtered index
+        Kokkos::View<output_data_type, output_array_layout, output_memory_space> extracted_view("extracted view", 
+                index_data_host.extent(0), input_data_host.extent(1));
+    
+        // Loop through all the entries of index data
+        for (int i=0; i<index_data_host.extent(0); i++) {
+            for (int j=0; j<input_data_host.extent(1); j++) {
+                extracted_view(i, j) = input_data_host(index_data_host(i), j);
+            }
         }
+    
+        // Then copy it back out - either to host or device space based on user's request
+        typedef Kokkos::View<output_data_type, output_array_layout, output_memory_space> output_view_type;
+        output_view_type extracted_view_output("output extracted view", extracted_view.extent(0), extracted_view.extent(1));
+        Kokkos::deep_copy(extracted_view_output, extracted_view);
+        Kokkos::fence();
+    
+        return extracted_view_output;
     }
 
-    // Then copy it back out - either to host or device space based on user's request
-    typedef Kokkos::View<output_data_type, output_array_layout, output_memory_space> output_view_type;
-    output_view_type extracted_view_output("output extracted view", extracted_view.extent(0), extracted_view.extent(1));
-    Kokkos::deep_copy(extracted_view_output, extracted_view);
-    Kokkos::fence();
+    template <typename output_memory_space, typename view_type_input_data, typename view_type_index_data,
+              enable_if_t<std::is_same<typename view_type_input_data::data_type, double*>::value
+                  ||std::is_same<typename view_type_input_data::data_type, int*>::value, int> = 0>
+    static Kokkos::View<double*, typename view_type_input_data::array_layout, output_memory_space> // shares layout of input by default
+            extractViewByIndex(view_type_input_data input_data_host_or_device, view_type_index_data index_data_host_or_device) {
 
-    return extracted_view_output;
-}
+        typedef typename view_type_input_data::data_type    output_data_type;
+        typedef typename view_type_input_data::array_layout output_array_layout;
+    
+        // Make view on the host for input data (does nothing if already on the host)
+        auto input_data_host = Kokkos::create_mirror_view(input_data_host_or_device);
+        Kokkos::deep_copy(input_data_host, input_data_host_or_device);
+        Kokkos::fence();
+    
+        // Make view on the host for index data (does nothing if already on the host)
+        auto index_data_host = Kokkos::create_mirror_view(index_data_host_or_device);
+        Kokkos::deep_copy(index_data_host, index_data_host_or_device);
+        Kokkos::fence();
+    
+        // Create a new view to extract out the rows that belong to the filtered index
+        Kokkos::View<output_data_type, output_array_layout, output_memory_space> extracted_view("extracted view", 
+                index_data_host.extent(0));
+    
+        // Loop through all the entries of index data
+        for (int i=0; i<index_data_host.extent(0); i++) {
+            extracted_view(i) = input_data_host(index_data_host(i));
+        }
+    
+        // Then copy it back out - either to host or device space based on user's request
+        typedef Kokkos::View<output_data_type, output_array_layout, output_memory_space> output_view_type;
+        output_view_type extracted_view_output("output extracted view", extracted_view.extent(0));
+        Kokkos::deep_copy(extracted_view_output, extracted_view);
+        Kokkos::fence();
+    
+        return extracted_view_output;
+    }
+
+};
 
 // template <typename output_memory_space, typename view_type_input_data, typename output_array_layout = typename view_type_input_data::array_layout, typename index_type=int>
 // Kokkos::View<int*, output_array_layout, output_memory_space> // shares layout of input by default
