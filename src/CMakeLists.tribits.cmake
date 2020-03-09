44639a488daa1683a3761d820d355b2d04f90a5c
Jason M. Gates <jmgate@sandia.gov>
2020-03-09 09:36:42 -0600

    Fill Out src TriBITS File

--- src/CMakeLists.tribits.cmake
+++ src/CMakeLists.tribits.cmake
@@ -1,1 +1,37 @@
-# INSERT TRIBITS EQUIVALENT HERE
+#
+# A) Package-specific configuration options
+#
+
+tribits_configure_file(${PACKAGE_NAME}_config.hpp)
+
+#
+# B) Define the header and source files (and directories)
+#
+
+set(HEADERS "")
+set(SOURCES "")
+
+include_directories(${CMAKE_CURRENT_BINARY_DIR})
+
+set(HEADERS ${HEADERS}
+  ${CMAKE_CURRENT_BINARY_DIR}/${PACKAGE_NAME}_config.hpp
+  )
+
+include_directories(${CMAKE_CURRENT_SOURCE_DIR})
+
+append_glob(HEADERS ${CMAKE_CURRENT_SOURCE_DIR}/*.hpp)
+list(REMOVE_ITEM HEADERS
+  ${CMAKE_CURRENT_SOURCE_DIR}/Compadre_Manifold_Functions.hpp
+  )
+append_glob(HEADERS ${CMAKE_CURRENT_SOURCE_DIR}/basis/*.hpp)
+append_glob(SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/*.cpp)
+
+#
+# C) Define the targets for package's library(s)
+#
+
+tribits_add_library(
+  compadre
+  HEADERS ${HEADERS}
+  SOURCES ${SOURCES}
+  )
