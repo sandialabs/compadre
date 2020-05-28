a796bb2c28e7dc96436f92411abee6a6d0db6a0b
Paul Kuberry <pkuberry@gmail.com>
2020-05-28 12:25:13 -0600

    Added basis and constraints folder to Doxygen.
    Added description of scalar Taylor polynomial basis.

--- src/basis/Compadre_ScalarTaylorPolynomial.hpp
+++ src/basis/Compadre_ScalarTaylorPolynomial.hpp
@@ -4,18 +4,19 @@
 #include "Compadre_GMLS.hpp"
 
 namespace Compadre {
-
-
+/*!  \brief Definition of scalar Taylor polynomial basis
+   
+   For order P, the sum of the basis is defined by:
+   \li in 1D) \f[\sum_{n=0}^{n=P} \frac{(x/h)^n}{n!}\f]
+   \li in 2D) \f[\sum_{n=0}^{n=P} \sum_{k=0}^{k=n} \frac{(x/h)^{n-k}*(y/h)^k}{(n-k)!k!}\f]
+   \li in 3D) \f[\sum_{p=0}^{p=P} \sum_{k_1+k_2+k_3=n} \frac{(x/h)^{k_1}*(y/h)^{k_2}*(z/h)^{k_3}}{k_1!k_2!k_3!}\f]
+*/
 namespace ScalarTaylorPolynomialBasis {
-
-
-    // for order P, the sum of the basis is defined by
-    //
-    // (in 1D) \sum_{n=0}^{n=P} (x/h)^n / n!
-    // (in 2D) \sum_{n=0}^{n=P} \sum_{k=0}^{k=n} (x/h)^(n-k)*(y/h)^k / ((n-k)!k!)
-    // (in 3D) \sum_{p=0}^{p=P} \sum_{k1+k2+k3=n} (x/h)^k1*(y/h)^k2*(z/h)^k3 / (k1!k2!k3!)
-    //
-    
+   
+    /*! \brief Returns size of basis
+        \param degree               [in] - highest degree of polynomial
+        \param dimension            [in] - spatial dimension to evaluate
+    */
     KOKKOS_INLINE_FUNCTION
     int getSize(const int degree, const int dimension) {
         if (dimension == 3) return (degree+1)*(degree+2)*(degree+3)/6;
@@ -23,6 +24,20 @@ namespace ScalarTaylorPolynomialBasis {
         else return degree+1;
     }
 
+    /*! \brief Evaluates the scalar Taylor polynomial basis
+     *  delta[j] = weight_of_original_value * delta[j] + weight_of_new_value * (calculation of this function)
+        \param delta                [in/out] - scratch space that is allocated so that each thread has its own copy. Must be at least as large as the _basis_multipler*the dimension of the polynomial basis.
+        \param workspace            [in/out] - scratch space that is allocated so that each thread has its own copy. Must be at least as large as the _poly_order*the spatial dimension of the polynomial basis.
+        \param dimension                [in] - spatial dimension to evaluate
+        \param max_degree               [in] - highest degree of polynomial
+        \param h                        [in] - epsilon/window size
+        \param x                        [in] - x coordinate (already shifted by target)
+        \param y                        [in] - y coordinate (already shifted by target)
+        \param z                        [in] - z coordinate (already shifted by target)
+        \param starting_order           [in] - polynomial order to start with (default=0)
+        \param weight_of_original_value [in] - weighting to assign to original value in delta (default=0, replace, 1-increment current value)
+        \param weight_of_new_value      [in] - weighting to assign to new contribution        (default=1, add to/replace)
+    */
     KOKKOS_INLINE_FUNCTION
     void evaluate(const member_type& teamMember, double* delta, double* workspace, const int dimension, const int max_degree, const double h, const double x, const double y, const double z, const int starting_order = 0, const double weight_of_original_value = 0.0, const double weight_of_new_value = 1.0) {
         Kokkos::single(Kokkos::PerThread(teamMember), [&] () {
@@ -89,6 +104,21 @@ namespace ScalarTaylorPolynomialBasis {
         });
     }
 
+    /*! \brief Evaluates the first partial derivatives of scalar Taylor polynomial basis
+     *  delta[j] = weight_of_original_value * delta[j] + weight_of_new_value * (calculation of this function)
+        \param delta                [in/out] - scratch space that is allocated so that each thread has its own copy. Must be at least as large as the _basis_multipler*the dimension of the polynomial basis.
+        \param workspace            [in/out] - scratch space that is allocated so that each thread has its own copy. Must be at least as large as the _poly_order*the spatial dimension of the polynomial basis.
+        \param dimension                [in] - spatial dimension to evaluate
+        \param max_degree               [in] - highest degree of polynomial
+        \param partial_direction        [in] - direction in which to take partial derivative
+        \param h                        [in] - epsilon/window size
+        \param x                        [in] - x coordinate (already shifted by target)
+        \param y                        [in] - y coordinate (already shifted by target)
+        \param z                        [in] - z coordinate (already shifted by target)
+        \param starting_order           [in] - polynomial order to start with (default=0)
+        \param weight_of_original_value [in] - weighting to assign to original value in delta (default=0, replace, 1-increment current value)
+        \param weight_of_new_value      [in] - weighting to assign to new contribution        (default=1, add to/replace)
+    */
     KOKKOS_INLINE_FUNCTION
     void evaluatePartialDerivative(const member_type& teamMember, double* delta, double* workspace, const int dimension, const int max_degree, const int partial_direction, const double h, const double x, const double y, const double z, const int starting_order = 0, const double weight_of_original_value = 0.0, const double weight_of_new_value = 1.0) {
         Kokkos::single(Kokkos::PerThread(teamMember), [&] () {
@@ -184,6 +214,22 @@ namespace ScalarTaylorPolynomialBasis {
         });
     }
 
+    /*! \brief Evaluates the second partial derivatives of scalar Taylor polynomial basis
+     *  delta[j] = weight_of_original_value * delta[j] + weight_of_new_value * (calculation of this function)
+        \param delta                [in/out] - scratch space that is allocated so that each thread has its own copy. Must be at least as large as the _basis_multipler*the dimension of the polynomial basis.
+        \param workspace            [in/out] - scratch space that is allocated so that each thread has its own copy. Must be at least as large as the _poly_order*the spatial dimension of the polynomial basis.
+        \param dimension                [in] - spatial dimension to evaluate
+        \param max_degree               [in] - highest degree of polynomial
+        \param partial_direction_1      [in] - direction in which to take first partial derivative
+        \param partial_direction_2      [in] - direction in which to take second partial derivative
+        \param h                        [in] - epsilon/window size
+        \param x                        [in] - x coordinate (already shifted by target)
+        \param y                        [in] - y coordinate (already shifted by target)
+        \param z                        [in] - z coordinate (already shifted by target)
+        \param starting_order           [in] - polynomial order to start with (default=0)
+        \param weight_of_original_value [in] - weighting to assign to original value in delta (default=0, replace, 1-increment current value)
+        \param weight_of_new_value      [in] - weighting to assign to new contribution        (default=1, add to/replace)
+    */
     KOKKOS_INLINE_FUNCTION
     void evaluateSecondPartialDerivative(const member_type& teamMember, double* delta, double* workspace, const int dimension, const int max_degree, const int partial_direction_1, const int partial_direction_2, const double h, const double x, const double y, const double z, const int starting_order = 0, const double weight_of_original_value = 0.0, const double weight_of_new_value = 1.0) {
         Kokkos::single(Kokkos::PerThread(teamMember), [&] () {
