f43b4b28e16dcd05e3bbe1578f2b806d0a2428e9
Paul Kuberry <pakuber@sandia.gov>
2021-02-17 06:59:51 -0700

    Added KokkosParser gtests for default arguments.

--- examples/Compadre_UnitTests.cpp
+++ examples/Compadre_UnitTests.cpp
@@ -8,21 +8,54 @@
 #include <mpi.h>
 #endif
 
+#define ASSERT_NO_DEATH(statement) \
+ASSERT_EXIT({{ statement } ::exit(EXIT_SUCCESS); }, ::testing::ExitedWithCode(0), "")
+
+//
+// KokkosParser tests go here because they modify the Kokkos backend
+//
+TEST (KokkosInitialize, NoArgsGiven) { 
+    Kokkos::InitArguments args;
+    ASSERT_NO_DEATH({
+            // default constructor is hidden for KokkosParser
+            // but still visible from this test
+            auto kp = Compadre::KokkosParser(-1,-1,-1,-1,false);
+            kp.finalize();
+    });
+}
+TEST (KokkosInitialize, NoCommandLineArgsGiven) { 
+    std::vector<std::string> arguments = {"application"};
+    std::vector<char*> argv;
+    for (const auto& arg : arguments) {
+        argv.push_back((char*)arg.data());
+    }
+    argv.push_back(nullptr);
+    ASSERT_NO_DEATH({
+            auto kp = KokkosParser(argv.size()-1, argv.data());
+            kp.finalize();
+    });
+}
+
+
 // this provides main(),
 // but all other tests come from ./unittests/*.cpp
 int main(int argc, char **argv) {
-        ::testing::InitGoogleTest(&argc, argv);
 
     // initializes MPI (if available) with command line arguments given
     #ifdef COMPADRE_USE_MPI
     MPI_Init(&argc, &argv);
     #endif
 
+    ::testing::InitGoogleTest(&argc, argv);
+    ::testing::GTEST_FLAG(filter) = "Kokkos*";
+    int sig = RUN_ALL_TESTS();
+
     // initializes kokkos
     auto kp = KokkosParser(argc, argv, true);
 
     // execute all tests
-    const int sig = RUN_ALL_TESTS();
+    ::testing::GTEST_FLAG(filter) = "[Kokkos*]";
+    sig &= RUN_ALL_TESTS();
 
     // finalize Kokkos and MPI (if available)
     kp.finalize();
