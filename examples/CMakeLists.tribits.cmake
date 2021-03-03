5111889c8646ae23fec923cc49fbf6b65e77cb5b
Paul Kuberry <pakuber@sandia.gov>
2021-03-02 17:08:09 -0700

    Commented out unit tests for Trilinos
    Gtest does not properly link to the tests in the
    Tribits framework at the moment, so the unit tests
    are temporarily disabled.

--- examples/CMakeLists.tribits.cmake
+++ examples/CMakeLists.tribits.cmake
@@ -1,10 +1,10 @@
 include_directories(${CMAKE_CURRENT_SOURCE_DIR})
 
-tribits_add_executable(
-  UnitTests
-  SOURCES
-    Compadre_UnitTests.cpp
-  ) # end tributs_add_executable
+#tribits_add_executable(
+#  UnitTests
+#  SOURCES
+#    Compadre_UnitTests.cpp
+#  ) # end tribits_add_executable
 
 tribits_add_executable(
   GMLS_Host_Test
@@ -84,25 +84,25 @@ tribits_add_executable(
     NeighborSearchTest.cpp
   ) # end tribits_add_executable
 
-set(testName Compadre_Unit_Tests)
-tribits_add_test(
-  UnitTests
-  NAME
-    ${testName}
-  COMM serial mpi
-  NUM_MPI_PROCS 1
-  ADDED_TESTS_NAMES_OUT ${testName}_CREATED
-  ) # end tribits_add_test
-if (${testName}_CREATED)
-  set_tests_properties(
-    ${${testName}_CREATED}
-    PROPERTIES
-      LABELS
-        "UnitTest;unittest;Unit;unit"
-      TIMEOUT
-        60
-    ) # end set_tests_properties
-endif() # test created
+#set(testName Compadre_Unit_Tests)
+#tribits_add_test(
+#  UnitTests
+#  NAME
+#    ${testName}
+#  COMM serial mpi
+#  NUM_MPI_PROCS 1
+#  ADDED_TESTS_NAMES_OUT ${testName}_CREATED
+#  ) # end tribits_add_test
+#if (${testName}_CREATED)
+#  set_tests_properties(
+#    ${${testName}_CREATED}
+#    PROPERTIES
+#      LABELS
+#        "UnitTest;unittest;Unit;unit"
+#      TIMEOUT
+#        60
+#    ) # end set_tests_properties
+#endif() # test created
 
 # Host views tests for GMLS
 set(testName GMLS_Host_Dim3_QR)
