a5ee108b21b5bcf2b4af3421e95e35b621ae302d
Paul Kuberry <pakuber@sandia.gov>
2021-09-02 07:22:41 -0600

    Extended pycompadre_example_1d.py
    - New weighting kernels are now supported in GUI
    - Slider bar for 2 weighting kernel parameters now added (instead of 1)
    - Epsilon multiplier is now consistent between slider bar and initial
      calculation

--- examples/pycompadre_example_1d.py
+++ examples/pycompadre_example_1d.py
@@ -19,7 +19,7 @@ kp = pycompadre.KokkosParser()
 solver_type = 'QR'
 polynomial_order = 2
 input_dimensions = 1
-epsilon_multiplier = 2.4
+epsilon_multiplier = 1.6
 global weighting_type, function, num_data_points
 weighting_type = 'power'
 function = lambda x: pow(x,2)
@@ -30,9 +30,10 @@ x_pred = np.linspace(0,4,200)
 extra_sites_coords = np.atleast_2d(np.linspace(0,4,200)).T
 
 # get GMLS approximate at all x_pred, as well as reconstruction about attempt_center_about_coord
-def approximate(solver_type, porder, wpower, wtype, epsilon_multiplier, attempt_center_about_coord):
+def approximate(solver_type, porder, wpower0, wpower1, wtype, epsilon_multiplier, attempt_center_about_coord):
     gmls_obj=pycompadre.GMLS(porder, input_dimensions, solver_type, "STANDARD")
-    gmls_obj.setWeightingParameter(wpower)
+    gmls_obj.setWeightingParameter(wpower0,0)
+    gmls_obj.setWeightingParameter(wpower1,1)
     gmls_obj.setWeightingType(wtype)
     gmls_helper = pycompadre.ParticleHelper(gmls_obj)
     gmls_helper.generateKDTree(np.atleast_2d(x).T)
@@ -72,7 +73,7 @@ def approximate(solver_type, porder, wpower, wtype, epsilon_multiplier, attempt_
     return (y_pred, computed_answer, center_about_extra_idx, center_about_extra_coord, colors)
 
 # get initial data for plotting
-y_pred, computed_answer, center_about_extra_idx, center_about_extra_coord, colors = approximate(solver_type, polynomial_order, 3, 'power', epsilon_multiplier, 1.2)
+y_pred, computed_answer, center_about_extra_idx, center_about_extra_coord, colors = approximate(solver_type, polynomial_order, 2, 1, 'power', epsilon_multiplier, 1.2)
 # plot initial data
 fig, ax = plt.subplots()
 plt.subplots_adjust(left=0.25, bottom=0.25)
@@ -91,33 +92,35 @@ plt.legend((p,d,l,s), ('Data', 'GMLS', 'Local Polynomial Fit', 'Target Site'))
 # axes for sliders and radio buttons
 ax_location_check = plt.axes([0.10, 0.068, 0.15, 0.15], facecolor='white', frameon=False)
 ax_location = plt.axes([0.25, 0.125, 0.65, 0.03], facecolor='white')
-ax_weighting_power = plt.axes([0.7, 0.025, 0.2, 0.03], facecolor=axcolor)
+ax_weighting_power_0 = plt.axes([0.6175, 0.025, 0.075, 0.03], facecolor=axcolor)
+ax_weighting_power_1 = plt.axes([0.825, 0.025, 0.075, 0.03], facecolor=axcolor)
 ax_num_data_points = plt.axes([0.25, 0.025, 0.2, 0.03], facecolor=axcolor)
 ax_epsilon = plt.axes([0.7, 0.075, 0.2, 0.03])
 ax_polynomial_order = plt.axes([0.25, 0.075, 0.2, 0.03])
-ax_weighting_type = plt.axes([0.015, 0.25, 0.15, 0.15], facecolor=axcolor)
+ax_weighting_type = plt.axes([0.015, 0.20, 0.15, 0.20], facecolor=axcolor)
 ax_func_type = plt.axes([0.015, 0.40, 0.15, 0.15], facecolor=axcolor)
-ax_solver_type = plt.axes([0.015, 0.55, 0.15, 0.15], facecolor=axcolor)
+ax_solver_type = plt.axes([0.015, 0.55, 0.15, 0.20], facecolor=axcolor)
 
 # sliders
 sl_location_check = CheckButtons(ax_location_check, ["",], [True,])
 delta_f = 4.0/200
 sl_location = Slider(ax_location, 'Location', valmin=0.0, valmax=4.0, valinit=0.0, valstep=delta_f, color=None, initcolor='black')
 sl_location.set_val(1.2)
-sl_weighting_power = Slider(ax_weighting_power, 'Weighting P.', valmin=0, valmax=5, valinit=3, valstep=1)
+sl_weighting_power_0 = Slider(ax_weighting_power_0, 'Wgt. P(0)', valmin=0, valmax=6, valinit=2, valstep=1)
+sl_weighting_power_1 = Slider(ax_weighting_power_1, 'Wgt. P(1)', valmin=0, valmax=6, valinit=1, valstep=1)
 sl_num_data_points = Slider(ax_num_data_points, 'Number of Points', valmin=6, valmax=50, valinit=10, valstep=1)
 sl_epsilon = Slider(ax_epsilon, 'Epsilon Multipler', valmin=1.0001, valmax=5.0, valinit=1.6, valstep=.1)
 sl_polynomial_order = Slider(ax_polynomial_order, 'Polynomial Order', valmin=0, valmax=6, valinit=2, valstep=1)
 
 #radios
-rad_weighting_type = RadioButtons(ax_weighting_type, ('Power', 'Cubic Spl.', 'Gaussian'), active=0)
+rad_weighting_type = RadioButtons(ax_weighting_type, ('Power', 'Cubic Spl.', 'Cosine', 'Gaussian', 'Sigmoid'), active=0)
 rad_func_type = RadioButtons(ax_func_type, ('sin(x)', 'x*sin(20x)', 'x^2'), active=2)
 rad_solver_type = RadioButtons(ax_solver_type, ('QR', 'LU'), active=0)
 
 def update(val):
     sl_location.valinit=2.0
     global weighting_type
-    y_pred, computed_answer, center_about_extra_idx, center_about_extra_coord, colors = approximate(rad_solver_type.value_selected, sl_polynomial_order.val, sl_weighting_power.val, weighting_type, sl_epsilon.val, sl_location.val)
+    y_pred, computed_answer, center_about_extra_idx, center_about_extra_coord, colors = approximate(rad_solver_type.value_selected, sl_polynomial_order.val, sl_weighting_power_0.val, sl_weighting_power_1.val, weighting_type, sl_epsilon.val, sl_location.val)
     l.set_ydata(computed_answer)
     d.set_ydata(y_pred)
     s.set_offsets([center_about_extra_coord, computed_answer[center_about_extra_idx]])
@@ -126,7 +129,8 @@ def update(val):
     fig.canvas.draw_idle()
 # register objects using update
 sl_location.on_changed(update)
-sl_weighting_power.on_changed(update)
+sl_weighting_power_0.on_changed(update)
+sl_weighting_power_1.on_changed(update)
 sl_epsilon.on_changed(update)
 sl_polynomial_order.on_changed(update)
 rad_solver_type.on_clicked(update)
@@ -140,13 +144,24 @@ def updatepoints(val):
     x[:] = np.linspace(0,4,num_data_points)
 
 def weighting_type_update(label):
-    weighting_type_dict = {'Power': 'power', 'Cubic Spl.': 'cubicspline', 'Gaussian' : 'gaussian'}
+    weighting_type_dict = {'Power': 'power', 'Cubic Spl.': 'cubicspline', 'Cosine': 'cosine', 'Gaussian': 'gaussian', 'Sigmoid': 'sigmoid'}
     global weighting_type
     weighting_type = weighting_type_dict[label]
-    if (weighting_type=='power'):
-        ax_weighting_power.set_visible(True)
-    else:
-        ax_weighting_power.set_visible(False)
+    if (label=='Power'):
+        ax_weighting_power_0.set_visible(True)
+        ax_weighting_power_1.set_visible(True)
+    elif (label=='Cubic Spl.'):
+        ax_weighting_power_0.set_visible(False)
+        ax_weighting_power_1.set_visible(False)
+    elif (label=='Cosine'):
+        ax_weighting_power_0.set_visible(False)
+        ax_weighting_power_1.set_visible(False)
+    elif (label=='Gaussian'):
+        ax_weighting_power_0.set_visible(True)
+        ax_weighting_power_1.set_visible(False)
+    elif (label=='Sigmoid'):
+        ax_weighting_power_0.set_visible(True)
+        ax_weighting_power_1.set_visible(True)
     update(0)
 # register objects using weighting_type_update
 rad_weighting_type.on_clicked(weighting_type_update)
@@ -159,7 +174,7 @@ def changefunc(label):
         func_type_dict = {'sin(x)': lambda x: np.sin(x), 'x*sin(20x)': lambda x: x*np.sin(20*x), 'x^2' : lambda x: pow(x,2)}
         function = func_type_dict[label]
     y[:] = function(x)
-    y_pred, computed_answer, center_about_extra_idx, center_about_extra_coord, colors = approximate(rad_solver_type.value_selected, sl_polynomial_order.val, sl_weighting_power.val, weighting_type, sl_epsilon.val, sl_location.val)
+    y_pred, computed_answer, center_about_extra_idx, center_about_extra_coord, colors = approximate(rad_solver_type.value_selected, sl_polynomial_order.val, sl_weighting_power_0.val, sl_weighting_power_1.val, weighting_type, sl_epsilon.val, sl_location.val)
     l.set_ydata(computed_answer)
     d.set_ydata(y_pred)
     p.set_offsets(np.vstack((x,y)).T)
