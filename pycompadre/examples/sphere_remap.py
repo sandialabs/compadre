2283211f29db3ebf8da1b1d3741d211b1f0f2c28
Paul Kuberry <pakuber@sandia.gov>
2022-03-15 07:07:12 -0600

    Uncomment cell integrated example in sphere_example.py

--- pycompadre/examples/sphere_remap.py
+++ pycompadre/examples/sphere_remap.py
@@ -13,164 +13,155 @@ Requires a .nc file that contains all of the fields referenced in this file.
 '''
 class TestSphereRemap(KokkosTestCase):
 
-    #def test_cell_integrated_remap(self):
+    def test_cell_integrated_remap(self):
 
-    #    # initialize parameters
-    #    polynomial_order = 5
-    #    input_dimensions = 3
-    #    epsilon_multiplier = 1.6
-    #    weighting_type = 'power'
-    #    weighting_power = 3
+        dataset = Dataset('../../../test_data/grids/hodge_0.nc', "r", format="NETCDF4")
 
-    #    dataset = Dataset('../../../test_data/grids/hodge_0.nc', "r", format="NETCDF4")
+        THREE = 3 # number of vertices in a triangle
+        radius = 6371220.00
+        dimensions = dataset.dimensions
+        variables = dataset.variables
 
-    #    radius = 6371220.00
-    #    dimensions = dataset.dimensions
-    #    variables = dataset.variables
+        p_order = 2
+        # get cells and put averaged quantities on them
+        q_order = 2
 
-    #    p_order = 2
-    #    # get cells and put averaged quantities on them
-    #    q_order = 2
-    #    q_dim   = 2 # local manifold dimension
-    #    qp = pycompadre.Quadrature(q_order, q_dim, "TRI")
+        DIM = 3
+        assert DIM==3, "Only created for 3D problem with 2D manifolds (DIM==3)"
+        Q_DIM   = 2 # local manifold dimension
 
-    #    # make a triangle from this midpoint, previous midpoint, and centroid
-    #    qpoints = qp.getSites()
-    #    qweights = qp.getWeights()
+        qp = pycompadre.Quadrature(q_order, Q_DIM, "TRI")
 
-    #    nEOnC = dataset['nEdgesOnCell']
-    #    max_nEOnC = np.max(nEOnC)
-    #    min_nEOnC = np.min(nEOnC)
-    #    assert max_nEOnC==min_nEOnC, "Max and min edges on cells are not the same"
+        # make a triangle from this midpoint, previous midpoint, and centroid
+        qpoints = qp.getSites()
+        qweights = qp.getWeights()
 
-    #    dim = 3
+        nEOnC = dataset['nEdgesOnCell']
+        max_nEOnC = np.max(nEOnC)
+        min_nEOnC = np.min(nEOnC)
+        assert max_nEOnC==min_nEOnC, "Max and min edges on cells are not the same"
 
-    #    # loop over quantities
-    #    # get number of cells
-    #    vOnC = dataset['verticesOnCell']
-    #    xC = dataset['xCell']
-    #    yC = dataset['yCell']
-    #    zC = dataset['zCell']
-    #    xV = dataset['xVertex']
-    #    yV = dataset['yVertex']
-    #    zV = dataset['zVertex']
-    #    nCells = dataset.dimensions['nCells'].size
-    #    extra_data = np.zeros(shape=(nCells, dim*max_nEOnC), dtype='f8')
-    #    cell_points = np.zeros(shape=(nCells, dim), dtype='f8')
+        # loop over quantities
+        # get number of cells
+        vOnC = dataset['verticesOnCell']
+        xC = dataset['xCell']
+        yC = dataset['yCell']
+        zC = dataset['zCell']
+        xV = dataset['xVertex']
+        yV = dataset['yVertex']
+        zV = dataset['zVertex']
+        nCells = dataset.dimensions['nCells'].size
+        extra_data = np.zeros(shape=(nCells, DIM*max_nEOnC), dtype='f8')
+        cell_points = np.zeros(shape=(nCells, DIM), dtype='f8')
 
-    #    # need to produce data for the remap (get cell integrated data)
-    #    f = lambda x: (x[0]/radius)**2 - (x[1]/radius)**2 + (x[1]/radius)*(x[0]/radius) + 55.0 + (x[2]/radius)**2 - (x[0]/radius)*(x[2]/radius)
+        # need to produce data for the remap (get cell integrated data)
+        f = lambda x: (x[0]/radius)**2 - (x[1]/radius)**2 + (x[1]/radius)*(x[0]/radius) \
+                       + 55.0 + (x[2]/radius)**2 - (x[0]/radius)*(x[2]/radius)
 
-    #    # directions 
-    #    v = np.zeros(shape=(dim,3), dtype='f8') # 3 is from triangle nodes
-    #    in_field = np.zeros(shape=(nCells), dtype='f8')
-    #    computed_total_area = 0.0
-    #    ref_total_area = 0.0
-    #    for cell in range(nCells):
-    #        cell_points[cell,:] = np.asarray([xC[cell], yC[cell], zC[cell]], dtype='f8')
-    #        cell_area = 0.0
-    #        facet_cell_area = 0.0
-    #        ref_total_area += dataset['areaCell'][cell]
-    #        for local_vertex in range(max_nEOnC):
-    #            vertex_num = vOnC[cell, local_vertex]-1
-    #            extra_data[cell,local_vertex*dim:(local_vertex+1)*dim] = np.asarray([xV[vertex_num], yV[vertex_num], zV[vertex_num]], dtype='f8')
+        # directions 
+        v = np.zeros(shape=(DIM,3), dtype='f8') # 3 is from triangle nodes
+        in_field = np.zeros(shape=(nCells), dtype='f8')
+        computed_total_area = 0.0
+        ref_total_area = 0.0
+        for cell in range(nCells):
+            cell_points[cell,:] = np.asarray([xC[cell], yC[cell], zC[cell]], dtype='f8')
+            cell_area = 0.0
+            facet_cell_area = 0.0
+            ref_total_area += dataset['areaCell'][cell]
+            for local_vertex in range(max_nEOnC):
+                vertex_num = vOnC[cell, local_vertex]-1
+                extra_data[cell,local_vertex*DIM:(local_vertex+1)*DIM] = \
+                    np.asarray([xV[vertex_num], yV[vertex_num], zV[vertex_num]], dtype='f8')
 
-    #        for local_vertex in range(max_nEOnC):
-    #            v[:,0] = cell_points[cell,:].copy()
-    #            v[:,1] = extra_data[cell,local_vertex*dim:(local_vertex+1)*dim] - v[:,0]
-    #            alt_local_vertex = (local_vertex+1) % max_nEOnC
-    #            v[:,2] = extra_data[cell,alt_local_vertex*dim:(alt_local_vertex+1)*dim] - v[:,0]
-    #            facet_cell_area += 0.5*np.linalg.norm(np.cross(v[:,1], v[:,2]))
+            for local_vertex in range(max_nEOnC):
+                v[:,0] = cell_points[cell,:].copy()
+                v[:,1] = extra_data[cell,local_vertex*DIM:(local_vertex+1)*DIM] - v[:,0]
+                alt_local_vertex = (local_vertex+1) % max_nEOnC
+                v[:,2] = extra_data[cell,alt_local_vertex*DIM:(alt_local_vertex+1)*DIM] - v[:,0]
+                facet_cell_area += 0.5*np.linalg.norm(np.cross(v[:,1], v[:,2]))
 
-    #            result = 0.0
-    #            for i in range(len(qpoints)):
+                result = 0.0
+                for i in range(len(qpoints)):
 
-    #                unscaled_transformed_qp = v[:,0].copy()
-    #                for j in range(dim-1):
-    #                    unscaled_transformed_qp += qpoints[i][j] * v[:,j+1]
-    #                transformed_qp_norm = np.linalg.norm(unscaled_transformed_qp)
-    #                scaled_transformed_qp = unscaled_transformed_qp.copy() * radius / transformed_qp_norm
+                    unscaled_transformed_qp = v[:,0].copy()
+                    for j in range(DIM-1):
+                        unscaled_transformed_qp += qpoints[i][j] * v[:,j+1]
+                    transformed_qp_norm = np.linalg.norm(unscaled_transformed_qp)
+                    scaled_transformed_qp = unscaled_transformed_qp.copy() * radius / transformed_qp_norm
 
-    #                # u_qp = midpoint + r_qp[1]*(v_1-midpoint) + r_qp[2]*(v_2-midpoint)
-    #                # s_qp = u_qp * radius / norm(u_qp) = radius * u_qp / norm(u_qp)
-    #                #
-    #                # so G(:,i) is \partial{s_qp}/ \partial{r_qp[i]}
-    #                # where r_qp is reference quadrature point (R^2 in 2D manifold in R^3)
-    #                #
-    #                # G(:,i) = radius * ( \partial{u_qp}/\partial{r_qp[i]} * (\sum_m u_qp[k]^2)^{-1/2}
-    #                #          + u_qp * \partial{(\sum_m u_qp[k]^2)^{-1/2}}/\partial{r_qp[i]} )
-    #                #
-    #                #        = radius * ( T(:,i)/norm(u_qp) + u_qp*(-1/2)*(\sum_m u_qp[k]^2)^{-3/2}
-    #                #                              *2*(\sum_k u_qp[k]*\partial{u_qp[k]}/\partial{r_qp[i]}) )
-    #                #
-    #                #        = radius * ( T(:,i)/norm(u_qp) + u_qp*(-1/2)*(\sum_m u_qp[k]^2)^{-3/2}
-    #                #                              *2*(\sum_k u_qp[k]*T(k,i)) )
-    #                #
-    #                qp_norm_sq = transformed_qp_norm**2
-    #                G = v.copy() / transformed_qp_norm
-    #                for k in range(dim):
-    #                    G[:,1] += unscaled_transformed_qp*(-0.5)*pow(qp_norm_sq,-1.5) \
-    #                              *2*(unscaled_transformed_qp[k]*v[k,1]);
-    #                    G[:,2] += unscaled_transformed_qp*(-0.5)*pow(qp_norm_sq,-1.5) \
-    #                              *2*(unscaled_transformed_qp[k]*v[k,2]);
+                    # u_qp = midpoint + r_qp[1]*(v_1-midpoint) + r_qp[2]*(v_2-midpoint)
+                    # s_qp = u_qp * radius / norm(u_qp) = radius * u_qp / norm(u_qp)
+                    #
+                    # so G(:,i) is \partial{s_qp}/ \partial{r_qp[i]}
+                    # where r_qp is reference quadrature point (R^2 in 2D manifold in R^3)
+                    #
+                    # G(:,i) = radius * ( \partial{u_qp}/\partial{r_qp[i]} * (\sum_m u_qp[k]^2)^{-1/2}
+                    #          + u_qp * \partial{(\sum_m u_qp[k]^2)^{-1/2}}/\partial{r_qp[i]} )
+                    #
+                    #        = radius * ( T(:,i)/norm(u_qp) + u_qp*(-1/2)*(\sum_m u_qp[k]^2)^{-3/2}
+                    #                              *2*(\sum_k u_qp[k]*\partial{u_qp[k]}/\partial{r_qp[i]}) )
+                    #
+                    #        = radius * ( T(:,i)/norm(u_qp) + u_qp*(-1/2)*(\sum_m u_qp[k]^2)^{-3/2}
+                    #                              *2*(\sum_k u_qp[k]*T(k,i)) )
+                    #
+                    qp_norm_sq = transformed_qp_norm**2
+                    G = v.copy() / transformed_qp_norm
+                    for k in range(DIM):
+                        G[:,1] += unscaled_transformed_qp*(-0.5)*pow(qp_norm_sq,-1.5) \
+                                  *2*(unscaled_transformed_qp[k]*v[k,1]);
+                        G[:,2] += unscaled_transformed_qp*(-0.5)*pow(qp_norm_sq,-1.5) \
+                                  *2*(unscaled_transformed_qp[k]*v[k,2]);
 
-    #                # cross product gives stretch factor
-    #                scaling = radius**2 * np.linalg.norm(np.cross(G[:,1], G[:,2]))
-    #                #print(scaling, np.linalg.norm(np.cross(v[:,1], v[:,2])))
-    #                #print(G)
-    #                result += f(scaled_transformed_qp)*qweights[i]*scaling
-    #                #print(f(scaled_transformed_qp),qweights[i],scaling)
-    #                cell_area += qweights[i]*scaling
+                    # cross product gives stretch factor
+                    scaling = radius**2 * np.linalg.norm(np.cross(G[:,1], G[:,2]))
+                    result += f(scaled_transformed_qp)*qweights[i]*scaling
+                    cell_area += qweights[i]*scaling
 
-    #            in_field[cell] += result
-    #        #print(in_field[cell])
-    #        #print(cell_area)
-    #        #print(facet_cell_area, ref_cell_area)
-    #        computed_total_area += cell_area
+                in_field[cell] += result
+            computed_total_area += cell_area
 
-    #    print("AREA", computed_total_area, " vs ", ref_total_area)
-    #    print("SPHERE AREA:",str(4.0*3.141592653*(radius**2)))
-    #    #print(in_field)
-    #    #print(cell_points)
-    #    #gmls_obj=pycompadre.GMLS(pycompadre.ReconstructionSpace.VectorOfScalarClonesTaylorPolynomial, 
-    #    gmls_obj=pycompadre.GMLS(pycompadre.ReconstructionSpace.ScalarTaylorPolynomial, 
-    #                             pycompadre.SamplingFunctionals['ScalarFaceIntegralSample'], 
-    #                             p_order, 
-    #                             dim, 
-    #                             "QR", 
-    #                             "MANIFOLD",
-    #                             "NO_CONSTRAINT",
-    #                             p_order)
+        print("AREA", computed_total_area, " vs ", ref_total_area)
+        print("SPHERE AREA:",str(4.0*3.141592653*(radius**2)))
+        gmls_obj=pycompadre.GMLS(pycompadre.ReconstructionSpace.ScalarTaylorPolynomial, 
+                                 pycompadre.SamplingFunctionals['ScalarFaceIntegralSample'], 
+                                 p_order, 
+                                 DIM, 
+                                 "QR", 
+                                 "MANIFOLD",
+                                 "NO_CONSTRAINT",
+                                 p_order)
 
-    #    gmls_obj.setOrderOfQuadraturePoints(q_order)
-    #    gmls_obj.setDimensionOfQuadraturePoints(q_dim)
-    #    gmls_obj.setQuadratureType("TRI")
-    #    gmls_helper = pycompadre.ParticleHelper(gmls_obj)
-    #    gmls_helper.generateKDTree(cell_points)
-    #    gmls_obj.addTargets(pycompadre.TargetOperation.ScalarPointEvaluation)
-    #    gmls_obj.setTargetExtraData(extra_data)
-    #    gmls_obj.setSourceExtraData(extra_data)
+        gmls_obj.setOrderOfQuadraturePoints(q_order)
+        gmls_obj.setDimensionOfQuadraturePoints(Q_DIM)
+        gmls_obj.setQuadratureType("TRI")
+        gmls_helper = pycompadre.ParticleHelper(gmls_obj)
+        gmls_helper.generateKDTree(cell_points)
+        gmls_obj.addTargets(pycompadre.TargetOperation.ScalarPointEvaluation)
+        gmls_obj.setTargetExtraData(extra_data)
+        gmls_obj.setSourceExtraData(extra_data)
 
-    #    gmls_helper.generateNeighborListsFromKNNSearchAndSet(cell_points, p_order, dim-1, 2.2)
-    #    gmls_obj.generateAlphas(1, False)
+        gmls_helper.generateNeighborListsFromKNNSearchAndSet(cell_points, p_order, DIM-1, 2.2)
+        gmls_obj.generateAlphas(1, False)
 
-    #    exact_out_field = np.zeros(shape=(nCells), dtype='f8')
-    #    for cell in range(nCells):
-    #        exact_out_field[cell] = f(cell_points[cell,:])
-    #    out_field = gmls_helper.applyStencil(in_field, pycompadre.TargetOperation.ScalarPointEvaluation)
-    #    print('exact out:',exact_out_field)
-    #    print('computed out:',out_field)
-    #    print('diff:',out_field - exact_out_field)
+        exact_out_field = np.zeros(shape=(nCells), dtype='f8')
+        for cell in range(nCells):
+            exact_out_field[cell] = f(cell_points[cell,:])
+        out_field = gmls_helper.applyStencil(in_field, pycompadre.TargetOperation.ScalarPointEvaluation)
+        print('exact out:',exact_out_field)
+        print('computed out:',out_field)
+        print('diff:',out_field - exact_out_field)
 
-    #    # circle area should be 6.2886e-04 of whatever area of sphere is
+        # calculate error norm (l2)
+        print('Error norm:',np.linalg.norm(out_field-exact_out_field, axis=0)/np.sqrt(out_field.shape[0]))
 
-    #    del gmls_helper, gmls_obj
+        # clean up Kokkos dependent objects
+        del gmls_helper, gmls_obj
 
     def test_edge_integrated_remap(self):
 
-        TWO = 2 # number of vertices on an edge
-
         dataset = Dataset('../../../test_data/grids/hodge_0.nc', "r", format="NETCDF4")
+
+        TWO = 2 # number of vertices on an edge
 
         remap_type  = 1
         remap_types = (pycompadre.SamplingFunctionals['FaceNormalPointSample'], 
@@ -186,15 +177,17 @@ class TestSphereRemap(KokkosTestCase):
         q_order = 3
         if remap_type==0:
             q_order = 1 # point sample can be though of as 1 pt quadrature
+
+        DIM = 3
+        assert DIM==3, "Only created for 3D problem with 2D manifolds (DIM==3)"
         Q_DIM   = 1 # local manifold dimension
+
         qp = pycompadre.Quadrature(q_order, Q_DIM, "LINE")
 
         # make a triangle from this midpoint, previous midpoint, and centroid
         qpoints = qp.getSites()
         qweights = qp.getWeights()
 
-        DIM = 3
-        assert DIM==3, "Only created for 3D problem with 2D manifolds (DIM==3)"
 
         # fields needed from nc file
         vOnE = dataset['verticesOnEdge']
@@ -420,11 +413,6 @@ class TestSphereRemap(KokkosTestCase):
 
         print('computed out:',out_field)
         print('exact out:',exact_out_field)
-        #print('diff_0:',out_field[:,0] - exact_out_field[:,0])
-        #print('diff_1:',out_field[:,1] - exact_out_field[:,1])
-        #print('diff_2:',out_field[:,2] - exact_out_field[:,2])
-        #print(dataset['latEdge'][:])
-        #print(dataset['lonEdge'][:])
         print('diff:',out_field - exact_out_field)
         #with np.printoptions(threshold=np.inf):
         #    print('diff:',out_field - exact_out_field)
