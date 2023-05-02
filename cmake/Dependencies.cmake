269e1b9935116d74e2b5eb9cc3feddaa9a7b055e
Paul Kuberry <pakuber@sandia.gov>
2023-05-02 15:27:00 -0600

    Refactor Kokkos and Gtest dependency in Trilinos
    - Changes from using KokkosCore to Kokkos in Trilinos@14.1
    - Removes detect_kokkos_opts.cmake which is no longer needed
    - Adds Gtest as a test dependency for Compadre package in Trilinos
    - Renames UnitTests to CompadreGtests and skips these tests if
      Gtest is not found in Trilinos

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,6 +1,8 @@
 tribits_package_define_dependencies(
   LIB_REQUIRED_PACKAGES
-    KokkosCore KokkosContainers KokkosAlgorithms KokkosKernels
+    Kokkos KokkosKernels
   LIB_OPTIONAL_TPLS
     MPI CUDA
+  TEST_REQUIRED_PACKAGES
+    Gtest
   )
