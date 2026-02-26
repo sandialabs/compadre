7dd773025d4751c9a47eca7a3d20dd77a262cd93
Paul Kuberry <pakuber@sandia.gov>
2026-02-26 12:05:29 -0500

    Turn off parts of bob not needed

--- pycompadre/__init__.py
+++ pycompadre/__init__.py
@@ -1,4 +1,9 @@
 from ._pycompadre import *
 __doc__ = _pycompadre.__doc__
+try:
+    from ._build_info import Compadre_DEBUG
+except ImportError:
+    Compadre_DEBUG = "Debug status unknown"
+__doc__ += '\nCompadre_DEBUG: ' + str(Compadre_DEBUG)
 __version__ = _pycompadre.__version__
 __all__ = _pycompadre.__dict__
